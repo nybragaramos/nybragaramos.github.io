4ac085cb4a3f28df8481cdd95cba27b7aaffa677
nadjayanna <nadjayannabr@gmail.com>
2018-11-21 17:19:19 +0100

    feat: create navbar for desktop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className='outer-container'>
+        <header role='banner' className='main-banner'>
+          <a className='nav-bar-brand' href='/'><h1>Portfolio</h1></a>
+          <nav className='nav-bar'>
+              <ul className='nav-bar-menu'>
+                <li  className='nav-bar-menu-item'><a href="page1.html">My Bio</a></li>
+                <li  className='nav-bar-menu-item'><a href="page2.html">Skills</a></li>
+                <li  className='nav-bar-menu-item'><a href="page3.html">Projects</a></li>
+                <li  className='nav-bar-menu-item'><a href="page4.html">Contact</a></li>
+              </ul>
+          </nav>
         </header>
+        <main>
+        </main>
       </div>
     );
   }
